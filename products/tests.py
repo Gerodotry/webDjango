2e5124c49ba91e90638fb13cf510ec98a98bd9df
Gerodotry <60816964+Gerodotry@users.noreply.github.com>
2024-10-02 10:14:54 +0300

    Update tests.py

--- products/tests.py
+++ products/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
-from django.test import TestCase
 from django.urls import reverse
 
 class ProductViewTest(TestCase):
     def test_get_product(self):
-        response = self.client.get(reverse('get_product', args=[1]))
+        # Тестуємо ендпоінт для productId = 1
+        response = self.client.get(reverse('product_detail', args=[1]))
+        
+        # Перевіряємо, що статус код відповіді 200
         self.assertEqual(response.status_code, 200)
+
+        # Перевіряємо, що відповідь має правильний JSON формат
         expected_data = {
             "id": "1",
             "name": "1 name"
