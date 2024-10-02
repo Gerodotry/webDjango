f5eeaa953f86c380a1a26370348f9a0a7a36a0f6
Gerodotry <60816964+Gerodotry@users.noreply.github.com>
2024-10-02 10:10:02 +0300

    create tests.py

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ProductViewTest(TestCase):
+    def test_get_product(self):
+        response = self.client.get(reverse('get_product', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        expected_data = {
+            "id": "1",
+            "name": "1 name"
+        }
+        self.assertJSONEqual(response.content, expected_data)
